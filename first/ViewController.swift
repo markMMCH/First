c5faa72fdf01c281d15fee270be8d78687a6a60c
markMMCH <lyubik.chibukhchian@gmail.com>
2020-03-09 11:14:14 +0400

    Initial Commit

---
+++ first/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by Mark Chibukhchian on 3/9/20.
+//  Copyright © 2020 Mark Chibukhchian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
